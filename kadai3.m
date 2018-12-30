8829284666f8bb21778115486d0ae803f6f6096a
Jun Yoneyama <16ec401@ms.dendai.ac.jp>
2018-12-30 18:53:04 +0900

    Update kadai3.m

--- kadai3.m
+++ kadai3.m
@@ -1,11 +1,11 @@
 % 課題３　閾値処理
-% 閾値を4パターン設定し,閾値処理た画像を示せ．
+% 閾値を4パターン設定し,閾値処理をした画像を示せ．
 % 下記はサンプルプログラムである．
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
 clear; % 変数のオールクリア
 
-ORG=imread('Lenna.png'); % 原画像の入力
+ORG=imread('mandrill.jpg'); % 原画像の入力
 ORG= rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
 
 imagesc(ORG); colormap(gray); colorbar; % 画像の表示
